42ee084bb7aa46c9da31c2d7a8f451c8715350bd
Troy HARRIS <troy.harris@djt-tech.com>
2013-05-17 17:42:15 -0700

    Initial Commit

---
+++ THGridMenu DemoTests/THGridMenu_DemoTests.h
@@ -0,0 +1,13 @@
+//
+//  THGridMenu_DemoTests.h
+//  THGridMenu DemoTests
+//
+//  Created by Troy HARRIS on 5/17/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface THGridMenu_DemoTests : SenTestCase
+
+@end
---
+++ THGridMenu DemoTests/THGridMenu_DemoTests.m
@@ -0,0 +1,32 @@
+//
+//  THGridMenu_DemoTests.m
+//  THGridMenu DemoTests
+//
+//  Created by Troy HARRIS on 5/17/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import "THGridMenu_DemoTests.h"
+
+@implementation THGridMenu_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in THGridMenu DemoTests");
+}
+
+@end
