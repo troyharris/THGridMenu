7e105f86e5c074e09946d67d4b4a327f99173159
Troy HARRIS <troy.harris@djt-tech.com>
2013-05-31 09:09:00 -0700

    Fixed a bug where the scrollview contentSize was not tall enough when uneven rows were created.

--- THGridMenu.m
+++ THGridMenu.m
@@ -105,9 +105,9 @@
         _columnInc = 0;
         _yOffset = _yOffset + _rowHeight + _marginSize;
         _xOffset = _gutterSize;
-        self.contentSize = CGSizeMake(self.contentSize.width, _yOffset);
     } else {
         _xOffset = _xOffset + _marginSize + itemWidth;
+        self.contentSize = CGSizeMake(self.contentSize.width, _yOffset + _marginSize + _rowHeight);
     }
     return item;
 }
